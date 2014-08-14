325dcd3565856cd7574f65653f7476758a491618
François Kooman <fkooman@tuxed.net>
2014-08-14 09:53:47 +0200

    update make_release script

--- make_release.sh
+++ make_release.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-VERSION=$1
-NAME=php-lib-json
 
-git clone https://github.com/fkooman/${NAME}.git ${NAME}-${VERSION}
+if [ "$#" -eq 1 ]
+then
+    VERSION=$1
+else
+    VERSION=0.0.1
+fi
+
+ARCHIVE_NAME=php-fkooman-json
+REPO_NAME=php-lib-json
+
+git clone https://github.com/fkooman/${REPO_NAME}.git ${ARCHIVE_NAME}-${VERSION}
 (
-cd ${NAME}-${VERSION}
+cd ${ARCHIVE_NAME}-${VERSION}
 composer.phar install --no-dev
 )
-tar -cJf ${NAME}-${VERSION}.tar.xz ${NAME}-${VERSION}
+tar -cJf ${ARCHIVE_NAME}-${VERSION}.tar.xz ${ARCHIVE_NAME}-${VERSION}
